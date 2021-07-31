fd53ce113387d29fbb5af9d40cee77cb3cc174a8
natalyaKh <khrebtiivski@gmail.com>
2021-07-31 15:59:33 +0300

    init commit

---
+++ src/main/java/com/smilyk/cond/ConditionersVer2Application.java
@@ -0,0 +1,13 @@
+package com.smilyk.cond;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConditionersVer2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConditionersVer2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/smilyk/cond/ConditionersVer2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smilyk.cond;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConditionersVer2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
