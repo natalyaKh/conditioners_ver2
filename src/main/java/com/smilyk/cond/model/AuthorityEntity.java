136265d17e05614aacdd8a368bca38a3283e063f
natalyaKh <khrebtiivski@gmail.com>
2021-07-31 16:30:12 +0300

    add models user, roles, authorities

---
+++ src/main/java/com/smilyk/cond/model/AuthorityEntity.java
@@ -0,0 +1,52 @@
+package com.smilyk.cond.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import java.util.Collection;
+
+@Table(name = "authorities")
+@Entity
+public class AuthorityEntity extends BaseEntity{
+    @Column(nullable = false, length = 20)
+    String name;
+    @ManyToMany(mappedBy="authorities")
+    private Collection<RoleEntity> roles;
+
+    public AuthorityEntity(String name) {
+        this.name = name;
+    }
+
+    public AuthorityEntity() {
+    }
+
+    @Override
+    public String toString() {
+        return "AuthorityEntity{" +
+            "name='" + name + '\'' +
+            ", roles=" + roles +
+            '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Collection<RoleEntity> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Collection<RoleEntity> roles) {
+        this.roles = roles;
+    }
+
+    public AuthorityEntity(String name, Collection<RoleEntity> roles) {
+        this.name = name;
+        this.roles = roles;
+    }
+}
---
+++ src/main/java/com/smilyk/cond/model/BaseEntity.java
@@ -0,0 +1,25 @@
+package com.smilyk.cond.model;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public class BaseEntity {
+    /**
+     * The unique id of model.
+     */
+    @Id
+    @GeneratedValue(
+        strategy = GenerationType.AUTO)
+    private long id;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+}
