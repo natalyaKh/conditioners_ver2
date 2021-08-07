ca5a482c2b863a4852f6d383dedfa9fae37e57e7
natalyaKh <khrebtiivski@gmail.com>
2021-08-07 18:01:41 +0300

    add AuthorizationFilter

--- src/main/java/com/smilyk/cond/controllers/ManagerUserController.java
+++ src/main/java/com/smilyk/cond/controllers/ManagerUserController.java
@@ -8,6 +8,7 @@ import com.smilyk.cond.service.valid.ValidationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -18,6 +19,7 @@ import java.util.List;
  * class Controller for users with Role = ROLE_MANAGER
  * all other users don have access to this EndPoints
  */
+@PreAuthorize("hasRole('ROLE_MANAGER')")
 @RestController
 @RequestMapping("ver1/manager")
 public class ManagerUserController {
--- src/main/java/com/smilyk/cond/security/WebSecurity.java
+++ src/main/java/com/smilyk/cond/security/WebSecurity.java
@@ -2,14 +2,23 @@ package com.smilyk.cond.security;
 
 import com.smilyk.cond.repo.UserEntityRepository;
 import com.smilyk.cond.security.filtres.AuthenticationFilter;
+import com.smilyk.cond.security.filtres.AuthorizationFilter;
 import com.smilyk.cond.security.service.UserSecurityService;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
 public class WebSecurity extends WebSecurityConfigurerAdapter {
     private final UserSecurityService userService;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
@@ -21,13 +30,19 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
         this.userRepository = userRepository;
     }
 
+    /**
+     * security configuration of API
+     * @param http {@link HttpSecurity}
+     * @throws Exception
+     */
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
             .authorizeRequests()
             .anyRequest().authenticated()
             .and()
-            .addFilter(getAuthenticationFilter());
+            .addFilter(getAuthenticationFilter())
+            .addFilter(new AuthorizationFilter(authenticationManager(), userRepository));;
         http.headers().frameOptions().disable();
     }
 
@@ -43,6 +58,23 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userService).passwordEncoder(bCryptPasswordEncoder);
+    }
+    /**
+     * add cors
+     */
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        final CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowCredentials(true);
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
     }
 
 
